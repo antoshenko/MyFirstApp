508f4d729376f4681be2184f5cf1ac03901f89f4
Vadim A <vadym.antoshenko@gmail.com>
2022-04-17 20:20:22 +0300

    finished Dog.java

--- src/com/my/first/app/chapter3/p92_Dog/Dog.java
+++ src/com/my/first/app/chapter3/p92_Dog/Dog.java
@@ -1,4 +1,40 @@
 package com.my.first.app.chapter3.p92_Dog;
 
 public class Dog {
+    String name;
+
+    public static void main(String[] args) {
+        Dog dog1 = new Dog();
+        dog1.bark();
+        dog1.name = "Bart";
+
+        Dog[] myDogs = new Dog[3];
+        myDogs[0] = new Dog();
+        myDogs[1] = new Dog();
+        myDogs[2] = dog1;
+
+        myDogs[0].name = "Fred";
+        myDogs[1].name = "George";
+
+        System.out.print("The name last dog - ");
+        System.out.println(myDogs[2].name);
+
+        int x = 0;
+        while (x < myDogs.length) {
+            myDogs[x].bark();
+            x = x + 1;
+        }
+
+    }
+
+    public void bark() {
+        System.out.println(name + " said Gub!");
+    }
+
+    public void eat() {
+    }
+
+    public void chaseCat() {
+    }
+
 }
